5dd05e8d995bad4cc35efda530f14dd74905ff82
r0bbar <r0bbarh00d@gmail.com>
2022-05-30 12:12:46 +0800

    teststr to check max length of env var in Digitalocean

--- felinesoulmate_web_btn_only/src/MainMint.js
+++ felinesoulmate_web_btn_only/src/MainMint.js
@@ -14,20 +14,12 @@ const MainMint = ( { accounts, setAccounts } ) => {
         if (window.ethereum) {
             const { MerkleTree } = require('merkletreejs');
             const keccak256 = require('keccak256');
+
+            teststr = process.env.REACT_APP_TESTSTR;
+            console.log('teststr.length:', teststr.length);
             
             // Hardcoded here! If address not here, "getHexProof" will return null!!
-            let whitelistAddresses = process.env.REACT_APP_WHITELIST_ADDRESSES.split(',')
-            /*
-            let whitelistAddresses = [
-                "0x07fa8e4f4ab4e4b72a7efca08f494a566a0c8568",
-                "0x7ef96a71d50f8a8f0fffb342a8c02bb0e29981ba",
-                "0XDCAB482177A592E424D1C8318A464FC922E8DE40",
-                "0X6E21D37E07A6F7E53C7ACE372CEC63D4AE4B6BD0",
-                "0X09BAAB19FC77C19898140DADD30C4685C597620B",
-                "0XCC4C29997177253376528C05D3DF91CF2D69061A",
-                "0xdD870fA1b7C4700F2BD7f44238821C26f7392148"
-              ];
-            */
+            let whitelistAddresses = process.env.REACT_APP_WHITELIST_ADDRESSES.split(',');
             
             const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
             const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
