60e96b1f56765c9dac2ff3ed06d0c848894fb43b
r0bbar <r0bbarh00d@gmail.com>
2022-09-15 18:36:07 +0800

    update gas limit

--- felinesoulmate_web_btn_only/src/MainMint.js
+++ felinesoulmate_web_btn_only/src/MainMint.js
@@ -80,7 +80,7 @@ const MainMint = ( { accounts, setAccounts } ) => {
                         value: ethers.utils.parseEther(
                                 (nft_pric_eth * mintAmount).toString()
                             ),
-                        gasLimit: 50000
+                        gasLimit: 8000000
                     }
                 );
                 console.log("Response: ", response);
